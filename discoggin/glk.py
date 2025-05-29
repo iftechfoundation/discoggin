589236c533fc49af33cdc68c0367f2a9e6447709
Andrew Plotkin <zarf@ZarfLent.local>
2025-05-29 11:43:23 -0400

    Rearrange.

--- discoggin/glk.py
+++ discoggin/glk.py
@@ -1,16 +1,4 @@
 
-def create_metrics(width=None, height=None):
-    if not width:
-        width = 800
-    if not height:
-        height = 480
-    res = {
-        'width':width, 'height':height,
-        'gridcharwidth':10, 'gridcharheight':12,
-        'buffercharwidth':10, 'buffercharheight':12,
-    }
-    return res
-
 class GlkState:
     def __init__(self):
         # Lists of ContentLines
@@ -157,3 +145,15 @@ def extract_raw(line):
             ### hyperlink
     return res
 
+
+def create_metrics(width=None, height=None):
+    if not width:
+        width = 800
+    if not height:
+        height = 480
+    res = {
+        'width':width, 'height':height,
+        'gridcharwidth':10, 'gridcharheight':12,
+        'buffercharwidth':10, 'buffercharheight':12,
+    }
+    return res
