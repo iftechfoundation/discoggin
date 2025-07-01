b164a52b0f555396ccbf6618be8afe0377665a4a
Andrew Plotkin <zarf@ZarfLent.local>
2025-07-01 09:32:34 -0400

    JSON loader with extras.

--- discoggin/util.py
+++ discoggin/util.py
@@ -1,6 +1,11 @@
 import os, os.path
+import json
 
 def delete_flat_dir(path):
+    """Delete a directory and all the files it contains. This is *not*
+    recursive; if the directory contains subdirs, it will raise an
+    exception (before deleting anything).
+    """
     if not os.path.exists(path):
         return
     if os.path.isfile(path):
@@ -13,3 +18,15 @@ def delete_flat_dir(path):
         os.remove(ent.path)
     os.rmdir(path)
     
+def load_json(path):
+    """
+    Read and parse a JSON file. Allow for the possibility of JSONP
+    ("var foo = {...};").
+    """
+    with open(path) as fl:
+        dat = fl.read()
+    startpos = dat.index('{')
+    endpos = dat.rindex('}')
+    dat = dat[ startpos : endpos+1 ]
+    return json.loads(dat)
+
